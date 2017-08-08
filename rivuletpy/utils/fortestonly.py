c037f23bffac4d28a9a9a461ada49caececa64e9
77 <rgao0313@uni.sydney.edu.au>
2017-08-08 15:34:38 +1000

    updatedversion

--- rivuletpy/utils/fortestonly.py
+++ rivuletpy/utils/fortestonly.py
@@ -1,2 +1,19 @@
-for i in range(2,200,7):
-    print(i)
+import math
+# import numpy as np
+# stack = []
+# sample1=np.arange(-1,-13,-1).reshape((3,4))
+# sample2=np.arange(1,13).reshape((3,4))
+# print(sample1)
+# print(sample2)
+# stack.append(np.rot90(np.fliplr(np.flipud(sample1))))
+# print(stack)
+# stack.append(np.rot90(np.fliplr(np.flipud(sample2))))
+# print(stack)
+# out = np.dstack(stack)
+# print("out begins")
+# print(out)
+
+# def combined(directory):
+#     combined[]=loadtiff3d(directory)
+a=str(math.ceil(9.3))
+print(a)
--- rivuletpy/utils/readim.py
+++ rivuletpy/utils/readim.py
@@ -1,23 +1,37 @@
 from rivuletpy.utils.io import *
-img=loadimg("1.tif")
+import os, math
+inputpath="/home/vv/Desktop/new/1.tif"
+img=loadimg(inputpath)
+dirpath=inputpath.split(".")[0]
+os.mkdir(dirpath)
+savepath=dirpath+"/"
 def cropimg(cropx,cropy,img):
-    z,y,x=img.shape
-    print(z,y,x)
-    for i in range(0,y,cropy):
+    x,y,z=img.shape
+    print(x,y,z)
+    for i in range(cropy,y,cropy):
         #每一横行的输出
         for j in range(cropx,x,cropx):
-            print(img[3:4,i-cropy:i,j-cropx:j])
+            oneitem=img[j-cropx:j,i-cropy:i,:]
+            writetiff3d(savepath+str(i/cropy)+"_"+str(j/cropx)+"_"+str(oneitem.shape[0])+"_"+str(oneitem.shape[1])+".tif"
+, oneitem)
         #每一横行如果有剩的最后一个输出
         if(x%cropx!=0):
-            print(img[3:4,i-cropy:i,x-x%cropx:x])
+            linelast=img[x-x%cropx:x,i-cropy:i,:]
+            writetiff3d(savepath + str(i/cropy) +"_"+ str(j/cropx+1) + "_"+str(linelast.shape[0])+"_"+str(linelast.shape[1])+".tif"
+                        , linelast)
             print("one single line ended")
     #最后一行开始
     print("final line begins")
     if(y%cropy!=0):
         for k in range(cropx,x,cropx):
-            print(img[3:4,y-y%cropy:y,k-cropx:k])
-#行列均有剩的最后一个
+            lastline=img[k-cropx:k,y-y%cropy:y,:]
+            writetiff3d(savepath + str(i/cropy+1) + "_"+str(j/cropx) + "_"+str(lastline.shape[0])+"_"+str(lastline.shape[1])+".tif"
+                    , lastline)
+    #行列均有剩的最后一个
     print("lucky last one")
     if((y%cropy!=0)and(x%cropx!=0)):
-        print(img[3:4,y-y%cropy:y,x-x%cropx:x])
-cropimg(20,20,img)
+        lastone=(img[x-x%cropx:x,y-y%cropy:y,:])
+        writetiff3d(savepath + str(i/cropy+1) +"_"+ str(j/cropx+1) + "_"+str(lastone.shape[0])+"_"+str(lastone.shape[1])+".tif"
+                    , lastone)
+cropimg(50,50,img)
+
