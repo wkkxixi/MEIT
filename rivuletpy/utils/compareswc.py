3291031306393273252dbd7f4a5b5fd715bd99b2
rgao0313 <rgao0313@uni.sydney.edu.au>
2017-09-22 15:48:27 +1000

    updated coding for csv

--- rivuletpy/utils/compareswc.py
+++ rivuletpy/utils/compareswc.py
@@ -1,8 +1,61 @@
-from rivuletpy.utils.metrics import *
+# from rivuletpy.utils.metrics import *
+from collections import deque
+import numpy as np
+from scipy.spatial.distance import cdist
 from rivuletpy.utils.io import *
 from os.path import join
 import csv
 from glob import glob
+
+def precision_recall(swc1, swc2, dist1=4, dist2=4):
+    '''
+    Calculate the precision, recall and F1 score between swc1 and swc2 (ground truth)
+    It generates a new swc file with node types indicating the agreement between two input swc files
+    In the output swc file: node type - 1. the node is in both swc1 agree with swc2
+                                                        - 2. the node is in swc1, not in swc2 (over-traced)
+                                                        - 3. the node is in swc2, not in swc1 (under-traced)
+    target: The swc from the tracing method
+    gt: The swc from the ground truth
+    dist1: The distance to consider for precision
+    dist2: The distance to consider for recall
+    '''
+
+    TPCOLOUR, FPCOLOUR, FNCOLOUR  = 3, 2, 180 # COLOUR is the SWC node type defined for visualising in V3D
+    swc1lines=swc1.shape[0]
+    swc2lines=swc2.shape[0]
+    mindist1list=[]
+    mindist2list=[]
+    for line in range(swc1lines):
+        d = cdist(swc1[line:line+1, 2:5], swc2[:, 2:5])
+        smindist1 = d.min(axis=1)
+        mindist1list.append(smindist1)
+    for line2 in range(swc2lines):
+        d2=cdist(swc1[:, 2:5], swc2[line2:line2+1, 2:5])
+        smindist2 = d2.min(axis=0)
+        mindist2list.append(smindist2)
+    mindist1s=np.array(mindist1list)#no squeeze numpy
+    mindist2s=np.array(mindist2list)#no squeeze numpy
+    mindist1=np.squeeze(mindist1s)
+    mindist2=np.squeeze(mindist2s)
+    tp = (mindist1 < dist1).sum()
+    fp = swc1.shape[0] - tp
+    fn = (mindist2 > dist2).sum()
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    # Make the swc for visual comparison
+    swc1[mindist1 <= dist1, 1] = TPCOLOUR
+    swc1[mindist1 > dist1, 1] = FPCOLOUR
+    swc2_fn = swc2[mindist2 > dist2, :]
+    swc2_fn[:, 0] = swc2_fn[:, 0] + 100000
+    swc2_fn[:, -1] = swc2_fn[:, -1] + 100000
+    swc2_fn[:, 1] = FNCOLOUR
+    swc_compare = np.vstack((swc1, swc2_fn))
+    swc_compare[:, -2]  = 1
+
+    return (precision, recall, f1), swc_compare
+
 content='Precision\tRecall\tF1'
 datapath = '/home/rong/Documents/Gold166-JSON/'
 list=glob(datapath+"*/")
@@ -22,18 +75,3 @@ with open( datapath+'gao_compare.csv', "w") as csv_file:
     writer = csv.writer(csv_file)
     for line in lines:
         writer.writerow([line])
-# print('Precision: %.2f\tRecall: %.2f\tF1: %.2f\t' % prf)
-
-# M1, M2 = gaussian_distance(swc1, swc2, 3.0)
-# print('M1 MEAN: %.2f\tM2 MEAN: %.2f' % (M1.mean(), M2.mean()))
-
-# midx1, midx2 = connectivity_distance(swc1, swc2)
-# for i in midx1:
-#     swc1[i, 1] = 2
-#     swc1[i, 5] = 4
-#
-# saveswc(join(datapath, 'test.connect1.swc'), swc1)
-# for i in midx2:
-#     swc2[i, 1] = 2
-#     swc2[i, 5] = 4
-# saveswc(join(datapath, 'test.connect2.swc'), swc2)
--- rivuletpy/utils/fortestonly.py
+++ rivuletpy/utils/fortestonly.py
@@ -14,7 +14,6 @@ import os, math
 # print(sample1[-1,-2])
 # sample2=np.arange(1,37).reshape((3,4,3))
 # bis = sample1 > 3
-# x = np.array([1, 2, 3, 4])
 # y =x > 2
 # z=y.astype(int)
 # print("int",z)
@@ -408,4 +407,144 @@ from rivuletpy.utils.io import *
 #             print('small swcs are being combined')
 #             combinedswc(folder)
 #             print('combined successfully!')
+from collections import deque
+import numpy as np
+from scipy.spatial.distance import cdist
+from rivuletpy.utils.io import *
+# datapath='/home/rong/Documents/Gold166-JSON/cambridgemouse/'
+# swc1=loadswc(datapath+'2.swc')
+# swc2=loadswc(datapath+'2_100_100.swc')
+#
+#
+# def precision_recall(swc1, swc2, dist1=4, dist2=4):
+#     '''
+#     Calculate the precision, recall and F1 score between swc1 and swc2 (ground truth)
+#     It generates a new swc file with node types indicating the agreement between two input swc files
+#     In the output swc file: node type - 1. the node is in both swc1 agree with swc2
+#                                                         - 2. the node is in swc1, not in swc2 (over-traced)
+#                                                         - 3. the node is in swc2, not in swc1 (under-traced)
+#     target: The swc from the tracing method
+#     gt: The swc from the ground truth
+#     dist1: The distance to consider for precision
+#     dist2: The distance to consider for recall
+#     '''
+#
+#     TPCOLOUR, FPCOLOUR, FNCOLOUR  = 3, 2, 180 # COLOUR is the SWC node type defined for visualising in V3D
+#     swc1lines=swc1.shape[0]
+#     swc2lines=swc2.shape[0]
+#     mindist1list=[]
+#     mindist2list=[]
+#     for line in range(swc1lines):
+#         d = cdist(swc1[line:line+1, 2:5], swc2[:, 2:5])
+#         smindist1 = d.min(axis=1)
+#         mindist1list.append(smindist1)
+#     for line2 in range(swc2lines):
+#         d2=cdist(swc1[:, 2:5], swc2[line2:line2+1, 2:5])
+#         smindist2 = d2.min(axis=0)
+#         mindist2list.append(smindist2)
+#     mindist1s=np.array(mindist1list)#no squeeze numpy
+#     mindist2s=np.array(mindist2list)#no squeeze numpy
+#     mindist1=np.squeeze(mindist1s)
+#     mindist2=np.squeeze(mindist2s)
+#     tp = (mindist1 < dist1).sum()
+#     fp = swc1.shape[0] - tp
+#     fn = (mindist2 > dist2).sum()
+#     precision = tp / (tp + fp)
+#     recall = tp / (tp + fn)
+#     f1 = 2 * precision * recall / (precision + recall)
+#
+#     # Make the swc for visual comparison
+#     swc1[mindist1 <= dist1, 1] = TPCOLOUR
+#     swc1[mindist1 > dist1, 1] = FPCOLOUR
+#     swc2_fn = swc2[mindist2 > dist2, :]
+#     swc2_fn[:, 0] = swc2_fn[:, 0] + 100000
+#     swc2_fn[:, -1] = swc2_fn[:, -1] + 100000
+#     swc2_fn[:, 1] = FNCOLOUR
+#     swc_compare = np.vstack((swc1, swc2_fn))
+#     swc_compare[:, -2]  = 1
+#
+#     return (precision, recall, f1), swc_compare
+#
+# precision_recall(swc1,swc2)
+# prf, swc_compare = precision_recall(swc1, swc2)
+# print('Precision: %.2f\tRecall: %.2f\tF1: %.2f\t' % prf)
+# from rivuletpy.utils.metrics import *
+from collections import deque
+import numpy as np
+from scipy.spatial.distance import cdist
+from rivuletpy.utils.io import *
+from os.path import join
+import csv
+from glob import glob
 
+def precision_recall(swc1, swc2, dist1=4, dist2=4):
+    '''
+    Calculate the precision, recall and F1 score between swc1 and swc2 (ground truth)
+    It generates a new swc file with node types indicating the agreement between two input swc files
+    In the output swc file: node type - 1. the node is in both swc1 agree with swc2
+                                                        - 2. the node is in swc1, not in swc2 (over-traced)
+                                                        - 3. the node is in swc2, not in swc1 (under-traced)
+    target: The swc from the tracing method
+    gt: The swc from the ground truth
+    dist1: The distance to consider for precision
+    dist2: The distance to consider for recall
+    '''
+
+    TPCOLOUR, FPCOLOUR, FNCOLOUR  = 3, 2, 180 # COLOUR is the SWC node type defined for visualising in V3D
+    swc1lines=swc1.shape[0]
+    swc2lines=swc2.shape[0]
+    mindist1list=[]
+    mindist2list=[]
+    for line in range(swc1lines):
+        d = cdist(swc1[line:line+1, 2:5], swc2[:, 2:5])
+        smindist1 = d.min(axis=1)
+        mindist1list.append(smindist1)
+    for line2 in range(swc2lines):
+        d2=cdist(swc1[:, 2:5], swc2[line2:line2+1, 2:5])
+        smindist2 = d2.min(axis=0)
+        mindist2list.append(smindist2)
+    mindist1s=np.array(mindist1list)#no squeeze numpy
+    mindist2s=np.array(mindist2list)#no squeeze numpy
+    mindist1=np.squeeze(mindist1s)
+    mindist2=np.squeeze(mindist2s)
+    tp = (mindist1 < dist1).sum()
+    fp = swc1.shape[0] - tp
+    fn = (mindist2 > dist2).sum()
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    # Make the swc for visual comparison
+    swc1[mindist1 <= dist1, 1] = TPCOLOUR
+    swc1[mindist1 > dist1, 1] = FPCOLOUR
+    swc2_fn = swc2[mindist2 > dist2, :]
+    swc2_fn[:, 0] = swc2_fn[:, 0] + 100000
+    swc2_fn[:, -1] = swc2_fn[:, -1] + 100000
+    swc2_fn[:, 1] = FNCOLOUR
+    swc_compare = np.vstack((swc1, swc2_fn))
+    swc_compare[:, -2]  = 1
+
+    return (precision, recall, f1), swc_compare
+
+content='Path\tPrecision\tRecall\tF1'
+datapath = '/home/rong/Documents/Gold166-JSON/'
+list=glob(datapath+"*/")
+for folder in list:
+    for l in os.listdir(folder):
+        if l.split(".")[-1]=='swc':
+            if len(l.split("."))==2:
+                if '_' not in l:
+                    try:
+                        swc1 = loadswc(join(folder, l))
+                        swc2 = loadswc(join(folder, l.split('.')[-2]+'_100_100.swc'))
+                        precision_recall(swc1,swc2)
+                        prf, swc_compare = precision_recall(swc1, swc2)
+                        content=content+'\n'+folder+l+'%.2f\t%.2f\t%.2f' % prf
+                    except (IOError):
+                        pass
+lines=content.split('\n')
+
+with open( datapath+'gao_compare.csv', "w") as csv_file:
+    writer = csv.writer(csv_file)
+    for line in lines:
+        writer.writerow([line])
