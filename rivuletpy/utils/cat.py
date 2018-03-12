64871d889767955a662e0e56756a6ef23e655359
wkkxixi <emilywang9147@gmail.com>
2018-03-12 12:18:19 +1100

    cat

--- rivuletpy/utils/cat.py
+++ rivuletpy/utils/cat.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 import fnmatch
 import multiprocessing as mp
-
+import shutil
+import h5py
 def loadimg(file):
     if file.endswith('.mat'):
         filecont = sio.loadmat(file)
@@ -332,6 +333,117 @@ def checkz(resolution, val):
                     if img.shape[2] != val:
                         print('abnormal: ' + str(val))
                         f.write(folder+y+'/'+x+'/' + z + '\n')
-cropz('RES(27300x17206x4923)')
+
+
+
+def cropy_batch(resolution, batch):
+    folder = '/Volumes/dh/heng_wang/' + resolution + '/'
+    crop_y = os.listdir(folder)
+    img = None
+    count = 1
+    with open(folder + 'info.txt', 'a') as f:
+        for y in crop_y:
+            if len(y) != 6:
+                continue
+            print(str(count) + ': ' + y)  
+            crop_x = os.listdir(folder+y)
+            for x in crop_x:
+                if not fnmatch.fnmatch(x, y + '_x.tif'):
+                    continue
+                print(x)
+                tmp = loadimg(folder+y+'/'+y + '_x.tif')
+                f.write(str(tmp.shape) + '\n')
+                if img is not None:
+                    img = np.append(tmp,img,axis=1)
+                else:
+                    img = tmp 
+            if count%batch == 0:
+                writetiff3d(folder  + resolution + '_whole_' + str(batch) + '.tif',img)
+                img = None
+                f.write('-----\nbatch ' + str(count/batch) + ': ' + folder  + resolution + '_whole_' + str(batch) + '.tif has been saved\n')
+                f.write('and the shape is ' + str(img.shape) + '\n-----\n')
+            count += 1
+        
+    
+
+def group_files(folder, resolution):
+    folder = folder + resolution + '/'
+    crop_y = os.listdir(folder)
+    count = 1
+    new_folder = '/Users/wonh/Desktop/' + resolution + '_y'
+    os.makedirs(new_folder)
+    for y in crop_y:
+        if len(y) != 6:
+            continue
+        print(str(count) + ': ' + y)
+        # crop_x = os.listdir(folder + y)
+        # for x in crop_x:
+        #     if fnmatch.fnmatch(x, y + '_x.tif'):
+        #         shutil.copy(folder + y + '/' + x, new_folder + '/' + resolution + '_whole_' + str(count) + '.tif')
+        #         print(str(count) + ': ' + x)
+        #         break
+        count += 1
+def piece_after_z(folder, resolution, y, x, log_path):
+    crop_z = os.listdir(folder + resolution + '/' + y + '/' + x)
+    z_img = None
+    with open(log_path, 'a') as f:
+        for z in crop_z:
+            if len(x) !=  :# to do
+                continue
+            f.write('>>>>>> ' + str(z) + ': /n')
+            tmp = loadtiff3d(folder + resolution + '/' + y + 'x' +'/' + z)
+            f.write('>>>>>> ' + str(tmp.shape) + '/n')
+            if z_img is None:
+                z_img = tmp
+            else:
+                z_img = np.append(z_img, tmp, axis=2)
+        f.write('------/n')
+    return z_img
+
+    
+def piece_after_x(folder, resolution, y, log_path):
+    crop_x = os.listdir(folder + resolution + '/' + y)
+    x_img = None
+    with open(log_path, 'a') as f:
+        for x in crop_x:
+            if len(x) !=  :# to do
+                continue
+            f.write('>>>> ' + str(x) + ': /n')
+            tmp = piece_after_z(folder, resolution, y, x, log_path)
+            f.write('>>>> ' + str(tmp.shape) + '/n')
+            if x_img is None:
+                x_img = tmp
+            else:
+                x_img = np.append(x_img, tmp, axis=0)
+        f.write('----/n')
+    return x_img
+
+def sober(folder, resolution):
+    crop_y = os.listdir(folder + resolution)
+    log_path = folder + resolution +'/combining_log.txt'
+    final_img = None
+    with open(log_path, 'a') as f:
+        f.write('The combining process of ' + resolution + ':\n')
+        for y in crop_y:
+            if len(y) != : #todo
+                continue
+            f.write('>> ' + str(y) + ': /n')
+            tmp = piece_after_x(folder, resolution, y, log_path)
+            f.write('>> ' + str(tmp.shape) + '/n')
+            if final_img is None:
+                final_img = tmp
+            else:
+                final_img = np.append(tmp, final_img, axis=1)
+        f.write('Final image shape is: ' + str(final_img.shape) + '\n')
+        f.write('----- Combine successfully -----\n')
+        
+    h5f = h5py.File(folder + resolution + '/' + resolution + '_hdf5.h5', 'w')
+    h5f.create_dataset('dataset_1', data=final_img)
+
+        
+
+
+# cropz('RES(27300x17206x4923)')
 # checkz('RES(3412x2150x615)', 205)
 # cropx('RES(6825x4301x1230)')
+group_files('/Volumes/dh/heng_wang/', 'RES(6825x4301x1230)')
