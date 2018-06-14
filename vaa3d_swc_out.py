b5042df5505e2a0dc5ab2f169fdcc4429aecb002
donghaozhangpeter <dzha9516@uni.sydney.edu.au>
2018-06-14 15:07:50 +1000

    loadswc is added

--- vaa3d_swc_out.py
+++ vaa3d_swc_out.py
@@ -1,4 +1,21 @@
 import os
+from rivuletpy.utils.compareswc import *
+
+def loadswc(filepath):
+    '''
+    Load swc file as a N X 7 numpy array
+    '''
+    swc = []
+    with open(filepath) as f:
+        lines = f.read().split("\n")
+        for l in lines:
+            if not l.startswith('#'):
+                cells = l.split(' ')
+                if len(cells) ==7:
+                    cells = [float(c) for c in cells]
+                    # cells[2:5] = [c-1 for c in cells[2:5]]
+                    swc.append(cells)
+    return np.array(swc)
 
 vaa3dpath = '/home/donghao/Desktop/wangheng/Vaa3D_Ubuntu_64bit_v3.200/vaa3d'
 goldenfolderpath = '/home/donghao/Desktop/wangheng/Gold166-JSON-meit/'
@@ -17,8 +34,15 @@ with open(goldenfolderpath + 'jsoninfo/detailedinfo.txt') as f:
 			print(neutubecmd)
 			counter_im = counter_im + 1
 			snakecmd = vaa3dpath + " -x snake -f snake_trace -i " + img_path
-			TreMapcmd = vaa3dpath + " -x TReMap -f snake_trace -i " + img_path + " -p 0 1 " + str(threshold) + " 0 1 0 5"
+			TreMapcmd = vaa3dpath + " -x TReMap -f trace_mip -i " + img_path + " -p 0 1 " + str(threshold) + " 0 1 0 5"
 			# os.system(neutubecmd)
 			# os.system(snakecmd)
-			os.system(TreMapcmd)
+			# os.system(TreMapcmd)
+			print(os.path.splitext(filename)[0])
+			gtswcpath = goldenfolderpath + os.path.splitext(filename)[0] + '.swc'
+			print(gtswcpath)
+			neutubeswcpath = goldenfolderpath + filename + '_neutube.swc'
+			print(neutubeswcpath)
+			gtswc = loadswc(gtswcpath)
+			neutubeswc = loadswc(neutubeswcpath)
 print('the total image number is ', counter_im)
