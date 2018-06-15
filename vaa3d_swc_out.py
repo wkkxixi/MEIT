b85df4dca1fc7c1fbf6a158a73cd6b3179dc7861
donghaozhangpeter <dzha9516@uni.sydney.edu.au>
2018-06-15 11:14:41 +1000

    precision recall of neutube

--- vaa3d_swc_out.py
+++ vaa3d_swc_out.py
@@ -29,20 +29,16 @@ with open(goldenfolderpath + 'jsoninfo/detailedinfo.txt') as f:
 			filename = item.split('\t')[0]
 			threshold = int(item.split('\t')[1])
 			img_path = goldenfolderpath + filename
-			print(filename)
 			neutubecmd = vaa3dpath + " -x neuTube -f neutube_trace -i " + img_path
-			print(neutubecmd)
 			counter_im = counter_im + 1
 			snakecmd = vaa3dpath + " -x snake -f snake_trace -i " + img_path
 			TreMapcmd = vaa3dpath + " -x TReMap -f trace_mip -i " + img_path + " -p 0 1 " + str(threshold) + " 0 1 0 5"
 			# os.system(neutubecmd)
 			# os.system(snakecmd)
 			# os.system(TreMapcmd)
-			print(os.path.splitext(filename)[0])
 			gtswcpath = goldenfolderpath + os.path.splitext(filename)[0] + '.swc'
-			print(gtswcpath)
 			neutubeswcpath = goldenfolderpath + filename + '_neutube.swc'
-			print(neutubeswcpath)
 			gtswc = loadswc(gtswcpath)
 			neutubeswc = loadswc(neutubeswcpath)
-print('the total image number is ', counter_im)
+			prf_accuracy, swc_compare_3_2 = precision_recall(neutubeswc, gtswc)
+			print(filename, prf_accuracy[0], prf_accuracy[1], prf_accuracy[2])
